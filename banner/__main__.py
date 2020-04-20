443646cdf9c470de68e304b6a3c9e04a0c978362
Mm2PL <jakis128@gmail.com>
2020-04-20 14:01:04 +0200

    Add more stats

--- banner/__main__.py
+++ banner/__main__.py
@@ -1,8 +1,11 @@
 import argparse
 # noinspection PyUnresolvedReferences
+import itertools
 import json
 import select
+import ssl
 import time
+from typing import Dict
 
 import twitchirc
 
@@ -69,11 +72,11 @@ def main(args):
         twitchirc.logging.log = lambda *a, **kwargs: None
         twitchirc.log = lambda *a, **kwargs: None
         print('{"type": "connecting"}')
-    read_connection = twitchirc.Connection('irc.chat.twitch.tv', port=6697, message_cooldown=0, secure=True)
-    read_connection.connect('justinfan123', 'justinfan123')
-    read_connection.cap_reqs(False)
-    read_connection.receive()
-    read_connection.process_messages(100)
+    # read_connection = twitchirc.Connection('irc.chat.twitch.tv', port=6697, message_cooldown=0, secure=True)
+    # read_connection.connect('justinfan123', 'justinfan123')
+    # read_connection.cap_reqs(False)
+    # read_connection.receive()
+    # read_connection.process_messages(100)
     conns = connections.create_connections(args.num_connections, args.auth, args.connection_wait)
     if args.is_automated:
         print('{"type": "connected"}')
@@ -95,22 +98,19 @@ def main(args):
     for num, ban in enumerate(list_parser.BanListIterator(args.ban_list_url, request_headers=headers)):
         if current_connection_number >= len(conns):
             current_connection_number = 0
-        # print(ban)
         conn = conns[current_connection_number]
         try:
             conn.socket.send(f'PRIVMSG #{channel} :{ban}\r\n'.encode())
-        except BrokenPipeError:
+        except (BrokenPipeError, ssl.SSLZeroReturnError):
             conns[current_connection_number] = connections.create_connections(1, args.auth,
                                                                               args.connection_wait)
             conn = conns[current_connection_number]
             conn.socket.send(f'PRIVMSG #{channel} :{ban}\r\n'.encode())
 
-        current_connection_number += 1
-
-        if num % 50 == 0:
+        if num % 50 == 0 and not args.enable_progress_reading:
             _print_progress_bar(num, args.length, args)
 
-        if num % (30 * current_connection_number) == 0:
+        if args.enable_progress_reading:
             while 1:
                 read, _, _ = select.select([conn.socket], [], [], 0)
                 if not read:
@@ -129,13 +129,20 @@ def main(args):
                                 f'Received data from #{current_connection_number}',
                                 log='\n'.join([repr(i) for i in msgs]))
 
-            conn_messages[current_connection_number] = msgs
+            conn_messages[current_connection_number].extend(msgs)
+        current_connection_number += 1
         time.sleep(args.sleep_time)
-    num_bans = num
-    for num, conn in enumerate(conns + [read_connection]):
-        _print_progress_bar(num, len(conns), args, comment='Collecting messages', log=f'Receiving from #{num}',
-                            message='')
 
+    num_bans = num
+    time.sleep(0.5)
+    last_collected = -1
+    for num, conn in enumerate(conns):
+        _print_progress_bar(num, len(conns), args, comment='Collecting messages',
+                            log=(
+                                '' if last_collected == -1 else f'Collected {last_collected} messages from #{num - 1}\n'
+                            ),
+                            message=f'Receiving from #{num}')
+        last_collected = 0
         while 1:
             read, _, _ = select.select([conn.socket], [], [], 0.5)
             if not read:
@@ -143,15 +150,40 @@ def main(args):
             o = conn.receive()
             if o == 'RECONNECT':
                 break
-            conn_messages[num] = conn.process_messages(10_000)
-    print(conn_messages)
+
+            data = conn.process_messages(10_000)
+            last_collected += len(data)
+            conn_messages[num].extend(data)
+
     end_time = time.time()
-    print(start_time, end_time)
-    print(end_time - start_time)
     _print_progress_bar(num_bans, num_bans, args, comment='Finished!',
                         log=f'Operation took {end_time - start_time:.2f}s')
     _print_progress_bar(num_bans, num_bans, args, comment='Finished!',
                         log=f'Speed: {num_bans / (end_time - start_time)}')
+
+    bans: Dict[str, bool] = {
+        # 'username': True,
+        # 'username2': False
+    }
+    for msg in itertools.chain.from_iterable(conn_messages.values()):
+        if isinstance(msg, twitchirc.Message) and f'NOTICE #{channel}' in msg.raw_data:
+            tags = {
+                i.split('=')[0]: i.split('=')[1] for i in msg.raw_data.split(' ')[0].lstrip('@').split(';')
+            }
+            if tags['msg-id'] in ['ban_success', 'already_banned']:
+                username = msg.raw_data.split(' ')[4].lstrip(':')
+                if username in bans:
+                    continue
+                bans[username] = 'ban_success' == tags['msg-id']
+
+    _print_progress_bar(num_bans, num_bans, args, comment='',
+                        log=f'Sent {num_bans} commands, got {len(bans)} responses')
+    new_bans = sum(bans.values())
+    _print_progress_bar(num_bans, num_bans, args, comment='',
+                        log=f'New bans: {new_bans}')
+
+    _print_progress_bar(num_bans, num_bans, args, comment='',
+                        log=f'Percentage of new bans from responses: {new_bans / len(bans) * 100:.2f}')
 
 
 if __name__ == '__main__':
@@ -165,6 +197,7 @@ if __name__ == '__main__':
     p.add_argument('-T', '--sleep-time', dest='sleep_time', default=0.03, type=float)
     p.add_argument('-c', '--connection-wait', dest='connection_wait', default=1.0, type=float)
     p.add_argument('-p', '--json', dest='is_automated', action='store_true')
+    p.add_argument('-nP', '--no-progress', dest='enable_progress_reading', action='store_false')
     aargs = p.parse_args(namespace=Args())
 
     main(aargs)
